ae38e922c76eaa996df1128657056113052ad1ce
Joppe <exozgames@gmail.com>
2019-02-02 15:20:18 +0100

    Fix app not closing

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const {app,BrowserWindow,ipcMain} = require('electron')
 let mainWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({width: 950, height: 400,transparent:true,frame:false,resizable:false})
+    mainWindow = new BrowserWindow({width: 950, height: 400,icon:__dirname + '/Krystal.png',transparent:true,frame:false,resizable:false})
     mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+    mainWindow.on('closed', () => { win = null })
+})
+
+
+app.on('window-all-closed', () => {
+    app.quit()
 })
 
 const client = require('discord-rich-presence')('541228950572105728')
